f8848923e66ab85092ad6272b70f62bcdc62f97f
Samuel-Aut <frantickillaace@gmail.com>
2024-09-30 13:35:27 +1300

    update css UI for Booklist

--- frontend/src/components/ShowBookList.tsx
+++ frontend/src/components/ShowBookList.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link';
 import BookCard from './BookCard';
 import { Book } from './Books';
 
-
-
 function ShowBookList() {
     const [books, setBooks] = useState<Book[]>([]);
     const [input, setInput] = useState('');
@@ -48,45 +46,40 @@ function ShowBookList() {
         fetchAllBooks();
     }, []);
 
-    const bookList = 
-        books.length === 0 
-            ? 'There are no book records!' 
-            : books.map((book, k) => <BookCard book={book} key={k} />);
-
     return (
-        <div className='ShowBookList'>
+        <div className='showBookList'>
             <div className='container'>
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <br />
-                        <h2 className='display-4 text-center'>Books List</h2>
-                    </div>
-                    <div className='col-md-11'>
-                        <Link href='/submission-form' className='btn btn-outline-warning float-right'>
+                <div className='header'>
+                    <h1 className='title'>Books List</h1>
+                    <div className='buttonGroup'>
+                        <Link href='/submission-form' className='linkButton'>
                             Submission
                         </Link>
-                        <Link href='/create-book' className='btn btn-outline-warning float-right' style={{ marginRight: '10px' }}>
+                        <Link href='/create-book' className='linkButton'>
                             + Add New Book
                         </Link>
-                        <form noValidate onSubmit={onSearch}>
-                            <button className='btn btn-outline-warning float-left' type='submit'>
-                                Search
-                            </button>
-                            <input
-                                type="text"
-                                placeholder="Book Title"
-                                name="title"
-                                className='form-control'
-                                required
-                                onChange={onChange}
-                            />
-                        </form>
-                        <br />
-                        <br />
-                        <hr />
                     </div>
                 </div>
-                <div className='list'>{bookList}</div>
+                <form className='searchForm' noValidate onSubmit={onSearch}>
+                    <input
+                        type="text"
+                        placeholder="Book Title"
+                        name="title"
+                        className='searchInput'
+                        required
+                        onChange={onChange}
+                    />
+                    <button className='searchButton' type='submit'>
+                        Search
+                    </button>
+                </form>
+                <div className='bookGrid'>
+                    {books.length === 0 ? (
+                        <p className="no-books">There are no book records!</p>
+                    ) : (
+                        books.map((book, k) => <BookCard book={book} key={k} />)
+                    )}
+                </div>
             </div>
         </div>
     );
